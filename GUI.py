c6b30fc94c748e53216cdb4d351a5ec92a7875f3
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-01-06 01:49:54 +0330

    complete gui window rendering complete gui events

--- GUI.py
+++ GUI.py
@@ -1,9 +1,33 @@
+import pygame
+import pygame.locals
+import sys
+
+import config
+from KeyboardPlayer import KeyboardPlayer
+from OnExitException import OnExitException
+
+
 class GUI:
-    def __init__(self):
-        pass
+    def __init__(self, wm):
+        pygame.init()
+
+        self.wm = wm
+        self.player = KeyboardPlayer()
+        self.screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
+        pygame.display.set_caption(config.WINDOW_NAME)
+        pygame.display.flip()
+
 
     def draw(self, wm):
         pass
 
     def get_events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                raise OnExitException()
+
+        return self.player.get_next_move(self.wm)
+
+    def close(self):
+        pygame.quit()
+        sys.exit()
