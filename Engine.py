1e0b465831d45f543174e54798e90e074a23140a
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-01-06 01:44:26 +0330

    implementing main loop

--- Engine.py
+++ Engine.py
@@ -1,6 +1,23 @@
+from GUI import GUI
+from OnExitException import OnExitException
+from WorldModel import WorldModel
+
+
 class Engine(object):
     def __init__(self):
-        pass
+        self.wm = WorldModel()
+        self.gui = GUI(self.wm)
+        self.running = True
 
     def run(self):
-        pass
+        while True:
+            self.gui.draw(self.wm)
+
+            event = None
+            try:
+                event = self.gui.get_events()
+            except OnExitException:
+                self.running = False
+                self.gui.close()
+
+            self.wm.update(event)
