013e92a1ab5c52f0710abd9a04ed34af3b547e95
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-01-23 01:24:29 +0330

    fix worldmodel to work with LearningEngine

--- worldmodel.py
+++ worldmodel.py
@@ -1,120 +1,137 @@
-from physicalobject import Pendulum, Cart
-from eventtype import*
-import config
 import math
+import random
+
+import config
+from actiontype import *
+from physicalobject import Pendulum, Cart
+from qlearning import State
+
+
 class WorldModel:
     def __init__(self):
         self.pendulum = Pendulum()
         self.cart = Cart()
 
-    def update(self, event):
-        G=config.GRAVITY
-        N=(self.cart.mass+self.pendulum.mass)*G
-        F_k=config.U_K *N
-        F_smax=config.U_S *N
-        a=0
-        if event==EventType.E_None:
-            if self.cart.velocity > 0:
-                a=-F_k / self.cart.mass
-                self.cart.velocity+=a * config.Time
-                if self.cart.velocity < 0 :
-                    self.cart.velocity =0
+    def update(self, action):
+        # calculate next state
+        nextState = self.compute_next_state(action)
 
-            if self.cart.velocity < 0:
-                a=F_k / self.cart.mass
-                self.cart.velocity+=a * config.Time
-                if self.cart.velocity > 0 :
-                    self.cart.velocity =0
-            self.cart.pos+=(self.cart.velocity *config.Time)+0.5*a*(config.Time**2)
-            if self.cart.pos > config.SPACE_WIDTH :
-                self.cart.velocity =0
-                self.cart.pos=config.SPACE_WIDTH
-            if self.cart.pos < 0 :
-                self.cart.velocity =0
-                self.cart.pos =0
+        # update current state
+        self.cart.pos = nextState.pos
+        self.cart.velocity = nextState.vel
+        self.pendulum.angle = nextState.angle
+        self.pendulum.w = nextState.w
 
-        if event==EventType.E_RIGHT:
-            if self.cart.velocity == 0:
-                if config.F_MOVEMENT > F_smax :
-                    a=(config.F_MOVEMENT - F_k) / self.cart.mass
-                    self.cart.velocity+=a * config.Time
-            if self.cart.velocity > 0 :
-                a=(config.F_MOVEMENT - F_k) / self.cart.mass
-                self.cart.velocity+=a * config.Time
-                if self.cart.velocity < 0 :
-                    self.cart.velocity=0
-            if self.cart.velocity < 0 :
-                a=(config.F_MOVEMENT - F_k) / self.cart.mass
-                self.cart.velocity+=a * config.Time
-                if self.cart.velocity > 0 :
-                    self.cart.velocity=0
-            self.cart.pos+=(self.cart.velocity *config.Time) + 0.5*a*(config.Time**2)
-            if self.cart.pos > config.SPACE_WIDTH :
-                self.cart.velocity =0
-                self.cart.pos=config.SPACE_WIDTH
-            if self.cart.pos < 0 :
-                self.cart.velocity =0
-                self.cart.pos =0
+    def reset_with_random_state(self):
+        # reset position & angle
+        self.cart.pos = random.uniform(40.0 / 100 * config.SPACE_WIDTH, 60.0 / 100 * config.SPACE_WIDTH)
+        self.pendulum.angle = random.uniform(-30.0, 30.0)
+        while self.pendulum.angle == 0:
+            self.pendulum.angle = random.randint(-30, 30)
 
-        if event==EventType.E_LEFT:
-            if self.cart.velocity ==0:
-                if config.F_MOVEMENT >F_smax :
-                    a=-(config.F_MOVEMENT-F_k) / self.cart.mass
-                    self.cart.velocity+=a * config.Time
-            if self.cart.velocity > 0 :
-                a=-(config.F_MOVEMENT - F_k) / self.cart.mass
-                self.cart.velocity+=a * config.Time
-                if self.cart.velocity < 0 :
-                    self.cart.velocity=0
-            if self.cart.velocity < 0 :
-                a=-(config.F_MOVEMENT - F_k) / self.cart.mass
-                self.cart.velocity+=a * config.Time
-                if self.cart.velocity > 0 :
-                    self.cart.velocity=0
-            self.cart.pos+=(self.cart.velocity *config.Time)+0.5*a*(config.Time**2)
-            if self.cart.pos > config.SPACE_WIDTH :
-                self.cart.velocity =0
-                self.cart.pos=config.SPACE_WIDTH
-            if self.cart.pos < 0 :
-                self.cart.velocity =0
-                self.cart.pos =0
-        F_air=config.U_air * math.fabs(self.pendulum.w) *self.pendulum.length
-        alpha_air=F_air / self.pendulum.mass
-        w_air=alpha_air *config.Time
-        '''
-        alpha=0
-        teta=math.fabs(math.radians(self.pendulum.angle))
-        if a>0:
-            if self.pendulum.angle >=0:
-                alpha=config.GRAVITY*self.pendulum.mass*math.sin(teta)+self.pendulum.mass*a*math.cos(teta)
-            if self.pendulum.angle <0:
-                alpha=self.pendulum.mass*a*math.cos(teta)-config.GRAVITY*self.pendulum.mass*math.sin(teta)
-        if a<0:
-            a*=-1
-            if self.pendulum.angle >=0:
-                alpha=config.GRAVITY*self.pendulum.mass*math.sin(teta)-self.pendulum.mass*a*math.cos(teta)
-            if self.pendulum.angle <0:
-                alpha=-config.GRAVITY*self.pendulum.mass*math.sin(teta)-self.pendulum.mass*a*math.cos(teta)
+        # reset W & velocity
+        self.cart.velocity = 0
+        self.pendulum.w = 0
 
-        if self.pendulum.w>0 :
-            self.pendulum.w+=alpha*config.Time -w_air
-        if self.pendulum.w<0 :
-            self.pendulum.w+=alpha*config.Time +w_air
-        if self.pendulum.w==0 :
-            self.pendulum.w=alpha*config.Time
+    def compute_next_state(self, action):
+        G = config.GRAVITY
+        N = (self.cart.mass + self.pendulum.mass) * G
+        F_k = config.U_K * N
+        F_smax = config.U_S * N
+        a = 0
 
-        self.pendulum.angle+=math.degrees(self.pendulum.w*config.Time)
-'''
-        alpha = 3 / 2 * (G * math.sin(math.radians(self.pendulum.angle)) + a * math.cos(math.radians(self.pendulum.angle))) / 1
+        # current state
+        cart_velocity = self.cart.velocity
+        cart_pos = self.cart.pos
+        pendulum_angle = self.pendulum.angle
+        pendulum_w = self.pendulum.w
+
+        if action == ActionType.ACT_NONE:
+            if cart_velocity > 0:
+                a = -F_k / self.cart.mass
+                cart_velocity += a * config.Time
+                if cart_velocity < 0:
+                    cart_velocity = 0
+
+            if cart_velocity < 0:
+                a = F_k / self.cart.mass
+                cart_velocity += a * config.Time
+                if cart_velocity > 0:
+                    cart_velocity = 0
+            cart_pos += (cart_velocity * config.Time) + 0.5 * a * (config.Time ** 2)
+            if cart_pos > config.SPACE_WIDTH:
+                cart_velocity = 0
+                cart_pos = config.SPACE_WIDTH
+            if cart_pos < 0:
+                cart_velocity = 0
+                cart_pos = 0
+
+        if action == ActionType.ACT_RIGHT:
+            if cart_velocity == 0:
+                if config.F_MOVEMENT > F_smax:
+                    a = (config.F_MOVEMENT - F_k) / self.cart.mass
+                    cart_velocity += a * config.Time
+            if cart_velocity > 0:
+                a = (config.F_MOVEMENT - F_k) / self.cart.mass
+                cart_velocity += a * config.Time
+                if cart_velocity < 0:
+                    cart_velocity = 0
+            if cart_velocity < 0:
+                a = (config.F_MOVEMENT - F_k) / self.cart.mass
+                cart_velocity += a * config.Time
+                if cart_velocity > 0:
+                    cart_velocity = 0
+            cart_pos += (cart_velocity * config.Time) + 0.5 * a * (config.Time ** 2)
+            if cart_pos > config.SPACE_WIDTH:
+                cart_velocity = 0
+                cart_pos = config.SPACE_WIDTH
+            if cart_pos < 0:
+                cart_velocity = 0
+                cart_pos = 0
+
+        if action == ActionType.ACT_LEFT:
+            if cart_velocity == 0:
+                if config.F_MOVEMENT > F_smax:
+                    a = -(config.F_MOVEMENT - F_k) / self.cart.mass
+                    cart_velocity += a * config.Time
+            if cart_velocity > 0:
+                a = -(config.F_MOVEMENT - F_k) / self.cart.mass
+                cart_velocity += a * config.Time
+                if cart_velocity < 0:
+                    cart_velocity = 0
+            if cart_velocity < 0:
+                a = -(config.F_MOVEMENT - F_k) / self.cart.mass
+                cart_velocity += a * config.Time
+                if cart_velocity > 0:
+                    cart_velocity = 0
+            cart_pos += (cart_velocity * config.Time) + 0.5 * a * (config.Time ** 2)
+            if cart_pos > config.SPACE_WIDTH:
+                cart_velocity = 0
+                cart_pos = config.SPACE_WIDTH
+            if cart_pos < 0:
+                cart_velocity = 0
+                cart_pos = 0
+
+        F_air = config.U_air * math.fabs(pendulum_w) * self.pendulum.length
+        alpha_air = F_air / self.pendulum.mass
+        w_air = alpha_air * config.Time
+        alpha = 3 / 2 * (
+            G * math.sin(math.radians(pendulum_angle)) + a * math.cos(math.radians(pendulum_angle))) / 1
         dw = alpha * config.Time
-        dAngle = 0.5 * alpha * config.Time ** 2 + self.pendulum.w * config.Time
-        if self.pendulum.w >0 :
-            self.pendulum.w += dw -w_air
-        if self.pendulum.w <0 :
-            self.pendulum.w += dw +w_air
-        if self.pendulum.w ==0 :
-            self.pendulum.w =dw
-        self.pendulum.angle += math.degrees(dAngle)
+        dAngle = 0.5 * alpha * config.Time ** 2 + pendulum_w * config.Time
+        if pendulum_w > 0:
+            pendulum_w += dw - w_air
+        if pendulum_w < 0:
+            pendulum_w += dw + w_air
+        if pendulum_w == 0:
+            pendulum_w = dw
+        pendulum_angle += math.degrees(dAngle)
+
+        return State(pendulum_angle, cart_pos, pendulum_w, cart_velocity)
+
+    def get_current_state(self):
+        return State(self.pendulum.angle, self.cart.pos, self.pendulum.w, self.cart.velocity)
+
     def get_pendulum(self):
         return self.pendulum
 
