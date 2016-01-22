1687c770c39202f42fee0765459187000bdccafd
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-01-23 01:21:58 +0330

    updated config for qleaning & learned_data

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+LEARNED_DATA = {
+    "path": 'learned_data/ld.pkl',
+    "dir": "learned_data",
+    "backup_prefix": "ld.pkl.back"
+}
+
 PENDULUM = {
     "mass": 1,
     "length": 0.8,
@@ -10,17 +16,22 @@ CART = {
     "drawable": "drawable/cart.png"
 }
 
-GRAVITY = 9.8
-
 SCREEN_WIDTH = 1076
 SCREEN_HEIGHT = 720
+DEGREE_STEP = 3
 WINDOW_NAME = "Inverted Pendulum"
 BACKGROUND_COLOR = (5, 187, 246)
+DEFAULT_Q = 0
+
+Q_GAMMA = 0.9
+Q_ALPHA = 0.5
 
 SPACE_WIDTH = 10.76
 SPACE_HEIGHT = 7.2
-F_MOVEMENT=90
-U_S=0.5
-U_K=0.5
-Time=0.001
-U_air=0.1
+
+GRAVITY = 9.8
+F_MOVEMENT = 90
+U_S = 0.5
+U_K = 0.3
+Time = 0.001
+U_air = 0.1
